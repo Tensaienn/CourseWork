4155c0340cd695828344c359fb35debf6bbc0ac8
Adennn <112881956+Tensaienn@users.noreply.github.com>
2025-04-14 20:54:01 +0700

    Zodiak Yunani

---
+++ Python/Mendeteksi Zodiak Yunani.py
@@ -0,0 +1,62 @@
+tanggal=int(input("Masukkan tanggal lahir kamu : "))
+bulan=str(input("Masukkan bulan lahir kamu (huruf kecil) : "))
+
+if (bulan=="maret" and tanggal >=21) or (bulan=="april" and tanggal <=19):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Aries")
+    
+elif (bulan=="april" and tanggal >=20) or (bulan=="mei" and tanggal <=20):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Taurus")
+    
+elif (bulan=="mei" and tanggal >=21) or (bulan=="juni" and tanggal <=20):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Gemini")
+    
+elif (bulan=="juni" and tanggal >=21) or (bulan=="juli" and tanggal <=22):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Cancer")
+    
+elif (bulan=="juli" and tanggal >=23) or (bulan=="agustus" and tanggal <=22):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Leo")
+    
+elif (bulan=="agustus" and tanggal >=23) or (bulan=="september" and tanggal <=22):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Virgo")
+    
+elif (bulan=="september" and tanggal >=23) or (bulan=="oktober" and tanggal <=22):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Libra")
+    
+elif (bulan=="oktober" and tanggal >=23) or (bulan=="november" and tanggal <=21):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Scorpio")
+    
+elif (bulan=="november" and tanggal >=22) or (bulan=="desember" and tanggal <=21):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Sagitarius")
+    
+elif (bulan=="desember" and tanggal >=22) or (bulan=="januari" and tanggal <=19):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Capricorn")
+    
+elif (bulan=="januari" and tanggal >=20) or (bulan=="februari" and tanggal <=18):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Aquarius")
+    
+elif (bulan=="februari" and tanggal >=19) or (bulan=="maret" and tanggal <=20):
+    print ("Tanggal kamu lahir : ",tanggal)
+    print ("Bulan kamu lahir : ",bulan)
+    print ("Zodiak kamu adalah Pisces")
