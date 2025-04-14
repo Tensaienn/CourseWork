adabcc40c2e68046f0afa8254b6bfc0393e6e896
Adennn <112881956+Tensaienn@users.noreply.github.com>
2025-04-14 20:56:58 +0700

    Zodiak Cina

---
+++ Python/Mendeteksi Zodiak Cina.py
@@ -0,0 +1,50 @@
+tahun=int(input("Masukkan tahun lahir kamu : "))
+sisa = (tahun - 2020) % 12
+
+if (sisa==0):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Tikus")
+    
+elif (sisa==1):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Kerbau")
+    
+elif (sisa==2):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Macan")
+    
+elif (sisa==3):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Kelinci")
+    
+elif (sisa==4):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Naga")
+    
+elif (sisa==5):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Ular")
+    
+elif (sisa==6):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Kuda")
+    
+elif (sisa==7):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Kambing")
+    
+elif (sisa==8):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Monyet")
+    
+elif (sisa==9):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Ayam")
+    
+elif (sisa==10):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Anjing")
+    
+elif (sisa==11):
+    print ("Tahun lahir kamu :",tahun)
+    print ("Zodiak kamu adalah Babi")
